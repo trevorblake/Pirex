275ea4ede844aa4e094950905e1314969d4e5972
Justin Roome <justinroome@csus.edu>
2021-11-10 12:05:04 -0800

    Added summarizeScrollPane and textArea to summarize page

--- Pirex/src/Display/Display.java
+++ Pirex/src/Display/Display.java
@@ -131,6 +131,15 @@ public class Display {
 		tabbedPane.setBackgroundAt(1, Color.WHITE);
 		summarize.setLayout(new GridLayout(1, 0, 0, 0));
 		
+		JScrollPane summarizeScrollPane = new JScrollPane();
+		summarizeScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		summarizeScrollPane.setViewportBorder(new LineBorder(new Color(0, 0, 0), 3));
+		summarize.add(summarizeScrollPane);
+		
+		JTextArea summarizeTextArea = new JTextArea();
+		summarizeTextArea.setForeground(Color.WHITE);
+		summarizeScrollPane.setViewportView(summarizeTextArea);
+		
 		//Code for "Load Documents" page
 
 		JPanel load = new JPanel();

95babd6dd33907cb204fe75d98f196303b962329
BigOreo99 <91107225+BigOreo99@users.noreply.github.com>
2021-11-12 18:05:39 -0800

    commit

--- Pirex/src/Display/Search.java
+++ Pirex/src/Display/Search.java
@@ -54,7 +54,7 @@ public class Search extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\amanj\\git\\Final-Project\\Pirex\\images\\p.png"));
 		setBackground(UIManager.getColor("Button.disabledShadow"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 600);
+		setBounds(100, 100, 504, 411);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);

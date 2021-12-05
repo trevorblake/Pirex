c3c4f52ae0666e68d8aadfd9b235891deb41d1a0
craga <craga@DESKTOP-SPAU6VD.attlocal.net>
2021-12-04 20:34:04 -0600

    added tests and more documents

---
+++ Pirex/src/Tests/DisplayTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DisplayTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");	}
+
+}
